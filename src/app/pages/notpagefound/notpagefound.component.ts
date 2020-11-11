1a2d9e4f231389e86d9bd32164ce8b8030b54e11
Geferman Pernia <geferman.pro@gmail.com>
2020-11-11 02:21:12 -0400

    g

--- src/app/pages/notpagefound/notpagefound.component.ts
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
   styleUrls: ['./notpagefound.component.css']
 })
-export class NotpagefoundComponent implements OnInit {
+export class NotpagefoundComponent {
 
   year  = new Date().getFullYear();
 }
